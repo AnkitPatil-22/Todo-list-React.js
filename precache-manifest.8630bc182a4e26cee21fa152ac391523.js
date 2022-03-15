43802cf20d0969eb150aa6c5533aa3a7a50c1f44
AnkitPatil-22 <ankitpatil2202@gmail.com>
2022-03-15 16:28:16 +0530

    Updates

---
+++ precache-manifest.8630bc182a4e26cee21fa152ac391523.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de686e89ee3a70b52ec5f46b6d45f622",
+    "url": "/Todo-list-React.js/index.html"
+  },
+  {
+    "revision": "5a6035e60db09faea70f",
+    "url": "/Todo-list-React.js/static/js/2.3c779e7e.chunk.js"
+  },
+  {
+    "revision": "051f10552b3bc166bf48",
+    "url": "/Todo-list-React.js/static/js/main.63f7c017.chunk.js"
+  },
+  {
+    "revision": "e614819266f8662cbda8",
+    "url": "/Todo-list-React.js/static/js/runtime-main.d1825004.js"
+  }
+]);
